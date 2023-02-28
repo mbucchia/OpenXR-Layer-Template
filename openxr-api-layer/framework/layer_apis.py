b0f09ac5c693310f33aba8cfbff86a37866d5f58
Matthieu Bucchianeri <mbucchia@gmail.com>
2023-02-27 23:28:20 -0800

    Example for writing a basic head-locked overlay using D3D11.

--- openxr-api-layer/framework/layer_apis.py
+++ openxr-api-layer/framework/layer_apis.py
@@ -1,14 +1,16 @@
 # The list of OpenXR functions our layer will override.
 override_functions = [
     "xrGetSystem",
-    "xrCreateSession"
+    "xrEndFrame",
 ]
 
 # The list of OpenXR functions our layer will use from the runtime.
 # Might repeat entries from override_functions above.
 requested_functions = [
     "xrGetInstanceProperties",
-    "xrGetSystemProperties"
+    "xrGetSystemProperties",
+    "xrCreateReferenceSpace",
+    "xrDestroySpace"
 ]
 
 # The list of OpenXR extensions our layer will either override or use.
