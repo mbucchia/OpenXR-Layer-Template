4f3c969cf976a3a35f3f0094a62210e1bbbfa8ea
Matthieu Bucchianeri <mbucchia@gmail.com>
2023-07-29 16:10:58 -0700

    Minor updates to the utility functions.

--- openxr-api-layer/framework/util.h
+++ openxr-api-layer/framework/util.h
@@ -30,7 +30,7 @@ namespace xr {
         return fmt::format("{}.{}.{}", XR_VERSION_MAJOR(version), XR_VERSION_MINOR(version), XR_VERSION_PATCH(version));
     }
 
-    static inline std::string ToString(XrPosef pose) {
+    static inline std::string ToString(const XrPosef& pose) {
         return fmt::format("p: ({:.3f}, {:.3f}, {:.3f}), o:({:.3f}, {:.3f}, {:.3f}, {:.3f})",
                            pose.position.x,
                            pose.position.y,
@@ -41,17 +41,27 @@ namespace xr {
                            pose.orientation.w);
     }
 
-    static inline std::string ToString(XrFovf fov) {
+    static inline std::string ToString(const XrFovf& fov) {
         return fmt::format(
             "(l:{:.3f}, r:{:.3f}, u:{:.3f}, d:{:.3f})", fov.angleLeft, fov.angleRight, fov.angleUp, fov.angleDown);
     }
 
-    static inline std::string ToString(XrRect2Di rect) {
-        return fmt::format("x:{}, y:{}, w:{}, h:{}", rect.offset.x, rect.offset.y, rect.extent.width, rect.extent.height);
+    static inline std::string ToString(const XrVector3f& vec) {
+        return fmt::format("({:.3f}, {:.3f}, {:.3f})", vec.x, vec.y, vec.z);
     }
 
-    static inline std::string ToString(XrRect2Df rect) {
-        return fmt::format("x:{}, y:{}, w:{}, h:{}", rect.offset.x, rect.offset.y, rect.extent.width, rect.extent.height);
+    static inline std::string ToString(const XrVector2f& vec) {
+        return fmt::format("({:.3f}, {:.3f})", vec.x, vec.y);
+    }
+
+    static inline std::string ToString(const XrRect2Di& rect) {
+        return fmt::format(
+            "x:{}, y:{}, w:{}, h:{}", rect.offset.x, rect.offset.y, rect.extent.width, rect.extent.height);
+    }
+
+    static inline std::string ToString(const XrRect2Df& rect) {
+        return fmt::format(
+            "x:{}, y:{}, w:{}, h:{}", rect.offset.x, rect.offset.y, rect.extent.width, rect.extent.height);
     }
 
 } // namespace xr
